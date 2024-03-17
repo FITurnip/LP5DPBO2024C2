0238e7fcf446a64dc513300c32629e1e86182a4c
FITurnip <franklinirtr@gmail.com>
2024-03-17 23:25:02 +0700

    complete

---
+++ DataMahasiswa/src/Mahasiswa.java
@@ -0,0 +1,43 @@
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String jenisKelamin;
+    private String kondisiRilis;
+
+    public Mahasiswa(String nim, String nama, String jenisKelamin, String kondisiRilis) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.kondisiRilis = kondisiRilis;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public void setKondisiRilis(String kondisiRilis) {
+        this.kondisiRilis = kondisiRilis;
+    }
+
+    public String getNim() {
+        return this.nim;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public String getJenisKelamin() {
+        return this.jenisKelamin;
+    }
+
+    public String getKondisiRilis() { return this.kondisiRilis; }
+}
